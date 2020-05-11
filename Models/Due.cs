1671e56283243167869989b37280ed73b1ba5037
Shahir1630 <39626291+Shahir1630@users.noreply.github.com>
2020-05-11 21:51:54 +0600

    Add files via upload

---
+++ Models/Due.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Group03_PS.Models
+{
+   
+    public class Due
+    {
+        public String MonthName { get; set; }
+        public Student student { get; set; }
+        public StudentPayment studentPayment { get; set; }
+    }
+   
+}
